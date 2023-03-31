2358ab849398b8d6d1ae276361a9694f08195262
Stephen Newman <steve@newman.digital>
2023-03-31 21:25:58 +0100

    Naive implementation returning true.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 
 fn main() {
     let mut urls = HashSet::new();
+    urls.insert("https://www.reddit.com/r/rust/");
     urls.insert("https://www.manning.com/books/rust-in-action");
     urls.insert("https://www.amazon.co.uk/Rust-Action-Tim-McNamara/dp/1617294551");
 
