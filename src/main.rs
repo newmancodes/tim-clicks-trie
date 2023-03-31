6615d06afd62b1303f5901486db2199656814064
Stephen Newman <steve@newman.digital>
2023-03-31 21:24:21 +0100

    Naive implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let mut urls = HashSet::new();
+    urls.insert("https://www.manning.com/books/rust-in-action");
+    urls.insert("https://www.amazon.co.uk/Rust-Action-Tim-McNamara/dp/1617294551");
+
+    let contains_reddit = urls.contains("https://www.reddit.com/r/rust/");
+
+    println!("Does urls contain Reddit?\n {contains_reddit}");
 }
