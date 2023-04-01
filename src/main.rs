25d09180e25afa032b3dac59f3269e750dd3c205
Stephen Newman <steve@newman.digital>
2023-04-01 09:42:29 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,62 @@
-use std::collections::HashSet;
+use std::{collections::HashMap, hash::Hash};
+
+#[derive(Debug, Default)]
+struct Node {
+    at_end: bool,
+    children: HashMap<char, Node>,
+}
+
+#[derive(Debug, Default)]
+struct Trie {
+    root: Node,
+    len: usize,
+}
+
+impl Trie {
+    fn new() -> Self {
+        Trie::default()
+    }
+
+    fn insert(&mut self, text: &str) {
+        let mut current_node = &mut self.root;
+
+        for c in text.chars() {
+            current_node = current_node.children.entry(c).or_default();
+        }
+
+        current_node.at_end = true;
+        self.len += 1;
+    }
+
+    fn contains(&self, text: &str) -> bool {
+        let mut current_node = &self.root;
+
+        for c in text.chars() {
+            match current_node.children.get(&c) {
+                Some(node) => current_node = node,
+                None => return false,
+            }
+        }
+
+        false
+    }
+
+    fn len(&self) -> usize {
+        self.len
+    }
+}
 
 fn main() {
-    let mut urls = HashSet::new();
+    let mut urls = Trie::new();
+
+    println!("{urls:?}");
+    // let mut urls = HashSet::new();
     urls.insert("https://www.reddit.com/r/rust/");
     urls.insert("https://www.manning.com/books/rust-in-action");
     urls.insert("https://www.amazon.co.uk/Rust-Action-Tim-McNamara/dp/1617294551");
 
-    let contains_reddit = urls.contains("https://www.reddit.com/r/rust/");
+    let stub = "https://www";
+    let contains_stub = urls.contains(stub);
 
-    println!("Does urls contain Reddit?\n {contains_reddit}");
+    println!("Does urls contain {stub}?\n {contains_stub}");
 }
